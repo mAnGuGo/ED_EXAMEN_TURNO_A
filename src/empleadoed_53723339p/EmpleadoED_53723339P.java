8f6437b240ee4090c3958c3781055812275931ae
miguel <miigueelgg@gmail.com>
2022-01-17 15:57:33 +0100

    codigo sin javadoc DNI 53723339P

---
+++ src/empleadoed_53723339p/EmpleadoED_53723339P.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package empleadoed_53723339p;
+
+/**
+ *
+ * @author miigu
+ */
+public class EmpleadoED_53723339P {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
